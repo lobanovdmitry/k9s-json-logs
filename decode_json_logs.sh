1dfe8b9f0a7714bc4dfcd2519faef22166967d3e
Dmitry Lobanov <39547483+lobanovdmitry@users.noreply.github.com>
2025-04-14 19:05:19 +0300

    Update decode_json_logs.sh

--- decode_json_logs.sh
+++ decode_json_logs.sh
@@ -1,10 +1,5 @@
-#!/bin/zsh
+#!/bin/bash
 
 set -e
 
-if [ -t 1 ] ; then
-    kubectl logs "$@" 2>&1 | jq -CR '. as $line | try (fromjson) catch $line' | sed 's/\\n/\n/g; s/\\t/\t/g' | less -R -K
-else
-    kubectl logs "$@" | jq -R '. as $line | try (fromjson) catch $line'
-fi
-
+kubectl logs "$@" 2>&1 | jq -CR '. as $line | try (fromjson) catch $line' | sed 's/\\n/\n/g; s/\\t/\t/g' | less -R -K
