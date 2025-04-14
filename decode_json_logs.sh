14f3424805666402f3dfcdaaba29310d16a39de7
Dmitry Lobanov <39547483+lobanovdmitry@users.noreply.github.com>
2025-04-14 19:02:56 +0300

    Update decode_json_logs.sh

--- decode_json_logs.sh
+++ decode_json_logs.sh
@@ -1,11 +1,9 @@
 #!/bin/zsh
 
 set -e
-RED="\033[0;31m"
 
 if [ -t 1 ] ; then
-    # https://github.com/derailed/k9s/issues/1852#issuecomment-2004674600
-    kubectl logs "$@" 2>&1 | jq -cCR '. as $line | try (fromjson) catch $line' | less -R -K
+    kubectl logs "$@" 2>&1 | jq -CR '. as $line | try (fromjson) catch $line' | sed 's/\\n/\n/g; s/\\t/\t/g' | less -R -K
 else
     kubectl logs "$@" | jq -R '. as $line | try (fromjson) catch $line'
 fi
